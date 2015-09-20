180c3df680bcc9379870300eee8772eb1aabe33c
Armando Barajas <ajbarajas1@gmail.com>
2015-09-20 19:20:45 -0400

    added search bar with JavaScript alert

---
+++ CodeLouisvilleProject/scripts.js
@@ -0,0 +1,8 @@
+
+function updateTxt(field1)
+  {  
+    var field1 = document.getElementById(field1).value;  
+    var message =  ('You searched "' + field1 + '". Please come back when the site is finished for all of your renting needs in Louisville!')
+    alert(message);
+
+    }  
